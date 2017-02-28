6360f217ab380eea99eaa4b3aecb336b0a638064
jihy0205 <jihy0205@gmail.com>
2017-02-28 20:50:44 +0900

    Scripts 스킬 추가

---
+++ Scripts/Skill/CheerUpEffect.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheerUpEffect : MonoBehaviour {
+
+    public int durabillity=100;
+    public GameObject cheerUpEffect;
+    public bool cheerupOn;
+ //   public bool cheerupOff;
+
+	// Use this for initialization
+	void Start () {
+        
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(cheerupOn == true)
+        {
+            cheerUpEffect.SetActive(true);
+
+        }
+        else if(cheerupOn == false)
+        {
+            cheerUpEffect.SetActive(false);
+        }
+		
+	}
+}
