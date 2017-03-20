a7518ae6fe01fd60bbdc5b126aa20f93c4005dc5
jihy0205 <jihy0205@gmail.com>
2017-03-21 01:41:00 +0900

    Script 카메라 추가

---
+++ Scripts/Ctrl/CameraAsix.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAsix : MonoBehaviour {
+
+	public float CameraSpeed = 10.0f;
+	Vector3 MouseGap;
+	public float distance = 3.0f;
+	public bool Invert = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton (1)) {
+			MouseGap.y += Input.GetAxis ("Mouse X") * CameraSpeed;
+			if (Invert)
+				MouseGap.x += Input.GetAxis ("Mouse Y") * CameraSpeed;
+			else
+				MouseGap.x -= Input.GetAxis ("Mouse Y") * CameraSpeed;
+
+			MouseGap.x = Mathf.Clamp (MouseGap.x, -50f, 50f);
+		}
+		transform.rotation = Quaternion.Euler (MouseGap);
+		Transform camT = Camera.main.transform;
+
+		Vector3 tmp = transform.forward * -1;
+		distance += Input.GetAxis ("Mouse ScrollWheel") * -10;
+
+		tmp *= distance;
+		camT.position = tmp + transform.position;
+	}
+}
